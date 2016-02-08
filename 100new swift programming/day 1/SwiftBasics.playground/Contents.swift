188a913228e1ac603dcc069fe15bfc7d282befb0
Teodora Blindu <teodorablindu@gmail.com>
2016-02-08 17:50:17 +0100

    day 1 swift

---
+++ 100new swift programming/day 1/SwiftBasics.playground/Contents.swift
@@ -0,0 +1,27 @@
+// Variables
+
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Hello, there!"
+
+var number = 10
+
+number = 20
+
+// Constants
+
+let language = "Swift"
+
+// Naming Conventions
+
+// rule #1: Spaces not allowed example:
+
+let programminglanguage = "Objective-C"
+
+// rule #2: Use camelCase
+
+let favoriteProgrammingLanguage = "Swift"
+
+// dissallowed characters: starting with number, white spaces, special characters like !*, mathematical symbols
